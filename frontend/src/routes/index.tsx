078c3e5a4065afc35f0aa7169688f8bff6888cc3
chelseaa1808 <cta1@usf.edu>
2025-04-15 15:38:01 +0000

    login requirement increase

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import PathConstants from "./PathConstants";
 import RequireAuth from "@/components/RequireAuth";
-import ChatHistory from "@/components/ChatHistory";
 
-// Lazy-load pages
+// Lazy-loaded pages
 const About = React.lazy(() => import("../pages/About"));
 const Home = React.lazy(() => import("../pages/Home"));
 const ChatPage = React.lazy(() => import("../pages/ChatPage"));
@@ -15,34 +15,78 @@ const Register = React.lazy(() => import("../pages/UserRegisterPage"));
 const UserProfile = React.lazy(() => import("../pages/UserProfilePage"));
 const AdminDashboard = React.lazy(() => import("../pages/AdminDashboardPage"));
 
-// Optional: define route shape
-interface Route {
-  path: string;
-  element: JSX.Element;
-}
-
-
-const routes: Route[] = [
+const routes = [
   { path: PathConstants.ABOUT, element: <About /> },
   { path: PathConstants.HOME, element: <Home /> },
-  { path: PathConstants.BASE_CHAT, element: <ChatPage /> },
-  { path: PathConstants.NEW_CHAT, element: <ChatPage /> },
-  { path: PathConstants.CHAT_PAGE, element: <ChatPage /> },
-  { path: PathConstants.BOT_PAGE, element: <BotPage /> },
-  { path: PathConstants.CONVERSATION_PAGE, element: <ConversationPage /> },
-  { path: PathConstants.CHAT_HISTORY, element: <ChatHistoryPage /> },
   { path: PathConstants.LOGIN, element: <Login /> },
   { path: PathConstants.REGISTER, element: <Register /> },
-  { path: PathConstants.PROFILE, element: <UserProfile /> }, // Fix key
-  { path: PathConstants.ADMIN_DASHBOARD, element: <AdminDashboard /> },
-  {
-    path: "/chat",
+
+  // Protected routes wrapped in RequireAuth --Temporarily disabled chat page security
+ {/*} {
+    path: PathConstants.BASE_CHAT,
     element: (
       <RequireAuth>
-        <ChatHistory />
+        <ChatPage />
       </RequireAuth>
     ),
-  }
+  },
+  {
+    path: PathConstants.NEW_CHAT,
+    element: (
+      <RequireAuth>
+        <ChatPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: PathConstants.CHAT_PAGE,
+    element: (
+      <RequireAuth>
+        <ChatPage />
+      </RequireAuth>
+    ),
+  }, */}
+  {
+    path: PathConstants.BOT_PAGE,
+    element: (
+      <RequireAuth>
+        <BotPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: PathConstants.CONVERSATION_PAGE,
+    element: (
+      <RequireAuth>
+        <ConversationPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: PathConstants.CHAT_HISTORY,
+    element: (
+      <RequireAuth>
+        <ChatHistoryPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: PathConstants.PROFILE,
+    element: (
+      <RequireAuth>
+        <UserProfile />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: PathConstants.ADMIN_DASHBOARD,
+    element: (
+      <RequireAuth>
+        <AdminDashboard />
+      </RequireAuth>
+    ),
+  },
+  { path: "*", element: <Navigate to={PathConstants.HOME} /> },
 ];
 
 export default routes;
