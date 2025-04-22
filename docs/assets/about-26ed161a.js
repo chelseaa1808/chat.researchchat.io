736ff7ca6a169b7740047768b001b918c880d5c7
chelseaa1808 <cta1@usf.edu>
2025-04-22 19:25:28 +0000

    Deploy static site to GitHub Pages from main branch

---
+++ docs/assets/about-26ed161a.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o}from"./client-642ef077.js";import r from"./About-04ada017.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
