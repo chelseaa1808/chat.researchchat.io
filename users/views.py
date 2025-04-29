e976a18a2d71a2ac1d77a5ad043f8ac890c42060
chelseaa1808 <cta1@usf.edu>
2025-04-29 06:38:41 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -56,3 +56,10 @@ class CurrentUserView(APIView):
             "username": user.username,
             "email": user.email,
         })
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+        return response
