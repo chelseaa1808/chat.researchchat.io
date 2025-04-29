505f70d77a33a8b4b81cf4fd119ceae26265b3b0
chelseaa1808 <cta1@usf.edu>
2025-04-29 07:37:14 -0400

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,27 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password1']
+        )
+        return user
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
