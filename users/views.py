cf198d559730077bb51769655338d7f486b5755f
chelseaa1808 <cta1@usf.edu>
2025-04-29 07:46:51 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .jwt_serializers import CustomTokenObtainPairSerializer
-from rest_framework import status
+from rest_framework import status, generics
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
