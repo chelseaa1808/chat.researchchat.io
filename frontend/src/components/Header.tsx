f1cd255813c5760e1c2436479e180cb349d09cc2
chelseaa1808 <cta1@usf.edu>
2025-05-11 19:26:11 -0400

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store";
 import { Link } from "react-router-dom";
 import { logout } from "@/store/slices/authSlice"; 
-import ResearchChatLogo from "./ChattrLogo";
+import ResearchChatLogo from "./ChatLogo";
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
